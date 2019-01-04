943c316f7aadd1010b3bc2188460c1b161b91eb8
Charlie Graham <graham7828@gmail.com>
2019-01-03 19:14:22 -0600

    Update

--- chroot.sh
+++ chroot.sh
@@ -5,30 +5,45 @@ echo
 echo 'Installing grub, linux headers, wireless packages...'
 echo
 echo
-pacman -S grub-bios linux-headers wpa_supplicant wireless_tools openssh
+pacman -S grub-bios linux-headers wpa_supplicant wpa_actiond dialog wireless_tools openssh
 echo
 echo
 
 # Set locale, symlink to local time
 echo
-echo "Setting locale... (Did you just assume my language??)"
+echo
 echo
 echo 'en_US.UTF-8 UTF-8' >>/etc/locale.gen
 locale-gen
 echo
 echo
-echo "(But you didn't assume my location?)"
+echo
 echo
 ls /usr/share/zoneinfo/
 echo
+echo
+echo
+echo
 echo -n 'ENTER NAME OF REGION, EXACTLY AS IT APPEARS ABOVE: '
 read MYREGION
+echo
+echo
+echo
+echo
+echo
 echo
 echo
 ls /usr/share/zoneinfo/$MYREGION
 echo
 echo -n 'ENTER NAME OF CITY, EXACTLY AS IT APPEARS ABOVE: '
 read MYCITY
+echo
+echo
+echo
+echo
+echo
+echo
+echo
 echo
 echo
 
@@ -38,6 +53,9 @@ ln -sf /usr/share/zoneinfo/$MYREGION/$MYCITY /etc/localtime
 hwclock --systohc --utc
 systemctl enable sshd.service
 echo
+echo
+echo
+echo
 
 # Set root password
 echo
@@ -45,25 +63,42 @@ echo 'Set password for root: '
 echo
 passwd
 echo
+echo
+echo
+echo
+echo
 
 # Install grub
+echo
+echo
 echo
 echo
 lsblk
 echo
 echo
-echo -n 'Enter boot DISK, NOT PARTITION (i.e. sda or sdb, NOT sda1, sda2): '
 echo
+echo -n 'Enter boot DISK, NOT PARTITION (i.e. sda or sdb, NOT sda1, sda2): '
 read BOOTDISK
 echo
 echo
+echo
+echo
+echo
+echo
+echo
+echo
 echo "Installing grub..."
+echo
 echo
 grub-install --target=i386-pc --recheck /dev/$BOOTDISK
 cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Create user, password, change hostname
+echo
+echo
+echo
+echo
 echo
 echo
 echo -n "Enter desired username: "
@@ -73,21 +108,31 @@ useradd -m -G wheel -s /bin/bash $MYUSER
 echo
 echo
 echo -n "Enter password for $MYUSER"
-echo
 passwd $MYUSER
+echo
+echo
+echo
 echo
 echo
 echo -n "Enter desired hostname: "
 read MYHOST
 echo $MYHOST >/etc/hostname
+echo
+echo
+echo
+echo
+echo
+echo
 
 # Install purdy purdy packages
 echo
 echo
 echo "Installing more packages..."
+echo "NOTE: During installation, 'Running build hook: [block]' can take a while. It has not (necessarily...) stalled "
 echo
 echo
-pacman -S sudo i3 pulseaudio pulseaudio-alsa pavucontrol pamixer pasystray xorg xorg-xinit dialog redshift feh bc thunar powertop vim ntfs-3g unzip imagemagick xfce4-terminal chromium compton base-devel bash-completion ttf-ibm-plex ttf-dejavu adobe-source-han-serif-jp-fonts adobe-source-han-sans-jp-fonts git acpi scrot xorg-xinput calibre cmake curl deluge evince mpv youtube-dl thunar-volman fuse-common fuse2 gimp gstreamer mesa mkvtoolnix-cli gparted perl python3 python2 qt wget dmenu xorg-xbacklight
+# No I am not including neofetch
+pacman -S sudo i3 pulseaudio pulseaudio-alsa pavucontrol pamixer pasystray xf86-input-libinput mesa xorg xorg-xinit redshift feh bc thunar thunar-volman fuse2 fuse-common ntfs-3g unzip unrar imagemagick powertop vim xfce4-terminal chromium compton base-devel bash-completion ttf-ibm-plex ttf-dejavu git acpi scrot cmake curl deluge evince mpv youtube-dl mkvtoolnix-cli wget dmenu
 
 # Add user to wheel
 echo "## Allow members of group wheel to execute any command" >> /etc/sudoers
@@ -111,6 +156,48 @@ rm -r /home/$MYUSER/dotfiles
 chown -R $MYUSER /home/$MYUSER
 echo
 echo
+
+# Remove install scripts from root
+rm /chroot.sh
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo "INSTALLATION COMPLETE! :D"
 echo "You may now reboot your system and remove Arch install media."
-echo "After reboot, log into your created user normally. There is a short script in your home directory called finish.sh."
-echo "Run that that as root, and after installing video drivers you are free to startx!"
+echo "When you log into your user account, there will be an installation-notes file in your home directory. Read it."
+echo
+echo
+echo
+echo
+echo "I lied: video drivers are not installed by DBAARF quite yet. Install them now."
+echo "Here I am listing your display devices, followed by the pacman xf86-video drivers:"
+echo
+echo
+echo "Your display devices: "
+echo
+lspci | grep VGA
+echo
+echo
+echo
+echo
+echo
+echo "Pacman xf86-video drivers: "
+echo
+pacman -Ss | grep xf86-video
+echo
+echo
+echo
+echo
+echo
+echo
+echo
--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ read ROOTPART
 echo -n 'Enter name of /home partition: '
 read HOMEPART
 echo
-
 mkfs.ext4 /dev/$ROOTPART
 mkfs.ext4 /dev/$HOMEPART
 mount /dev/$ROOTPART /mnt
@@ -24,6 +23,22 @@ pacstrap -i /mnt base
 
 # Generate fstab
 genfstab -U -p /mnt >> /mnt/etc/fstab
+
+# Check for swap (if exists, add to fstab)
+if [ "$(sudo fdisk -l | grep swap | cut -d'/' -f3 | cut -d' ' -f1)" != ""  ]
+    then
+        echo
+        echo 'Swap partition detected: Adding to fstab...'
+        echo
+        SWAPPART=$(sudo fdisk -l | grep swap | cut -d'/' -f3 | cut -d' ' -f1)
+        mkswap /dev/$SWAPPART
+        MYUUID=$(sudo blkid | grep $SWAPPART | cut -d'"' -f2)
+        echo >>/mnt/etc/fstab
+        echo "# /dev/$SWAPPART" >>/mnt/etc/fstab
+        echo "UUID=$MYUUID     none        swap        defaults        0 0" >>/mnt/etc/fstab
+        swapon
+fi
+
 
 # Chroot into install
 cp postinstall.sh finish.sh chroot.sh /mnt
