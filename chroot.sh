27eca6e27faf3596589d22e3377fa99d1e12f853
drblythe <graham7828@gmail.com>
2019-02-02 01:07:58 -0600

    update

--- chroot.sh
+++ chroot.sh
@@ -69,7 +69,7 @@ echo $'\n'
 echo "Installing packages..."
 echo $'\n'
 # NO NEOFETCH!11!!!1
-pacman -Sy sudo i3 pulseaudio pulseaudio-alsa pavucontrol pamixer pasystray networkmanager network-manager-applet xf86-input-libinput mesa xorg xorg-xinit xorg-xbacklight redshift bc ranger w3m feh ntfs-3g dosfstools unzip unrar imagemagick htop tlp vim urxvt chromium thunderbird base-devel bash-completion ttf-ibm-plex ttf-dejavu adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts git acpi scrot cmake curl zathura zathura-djvu zathura-pdf-mupdf mpv youtube-dl mkvtoolnix-cli wget dmenu sysstat python python-requests
+pacman -Sy dialog grub linux-headers wpa_supplicant wpa_actiond wireless_tools openssh sudo i3 pulseaudio pulseaudio-alsa pavucontrol pamixer pasystray networkmanager network-manager-applet xf86-input-libinput mesa xorg xorg-xinit xorg-xbacklight redshift bc ranger w3m feh ntfs-3g dosfstools unzip unrar imagemagick htop tlp vim urxvt chromium thunderbird base-devel bash-completion ttf-ibm-plex ttf-dejavu adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts git acpi scrot cmake curl zathura zathura-djvu zathura-pdf-mupdf mpv youtube-dl mkvtoolnix-cli wget dmenu sysstat python python-requests
 
 
 
